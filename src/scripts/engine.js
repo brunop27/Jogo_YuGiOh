70448239fa239eb32cbe518bbef7b49b16ac15c6
Brunop27 <brunopinheiro715@gmail.com>
2023-12-04 07:16:33 -0300

    Adicionando audio de fundo, favicon e icone no mouse

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -194,6 +194,9 @@ function init(){
     showHiddenCardFieldsImages(false);
     drawCards(5,state.playerSides.player1);
     drawCards(5,state.playerSides.computer);
+
+    const bgm = document.getElementById("bgm");
+    bgm.play();
 };
 
 init();
